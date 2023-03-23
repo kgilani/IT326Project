4b01d1ae8f9ecc10b1befd93fec85bbc30bc78c8
kgilani <128659086+kgilani@users.noreply.github.com>
2023-03-22 21:41:53 -0500

    Update mealPlan.java

--- mealPlan.java
+++ mealPlan.java
@@ -1,2 +1,19 @@
 //meal plan class contains methods and variables pertaining to a collection of meals
 //extends meal
+public class mealPlan extends meal {
+    String name = "";
+    String description = "";
+
+    mealPlan(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public void addMeal{
+
+    }
+
+    public void deleteMeal{
+
+    }
+}
