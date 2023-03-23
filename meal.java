a27a00b2392fe0a628c71a9dbfe8ca36aec06f57
kgilani <128659086+kgilani@users.noreply.github.com>
2023-03-22 22:10:27 -0500

    Update meal.java

--- meal.java
+++ meal.java
@@ -1,2 +1,24 @@
 //meal class contains methods and variables pertaining to an individual meal composed of ingredients
 //extends ingredient and APIDelegator
+public class meal{
+    String name = "";
+    String description = "";
+    int totalCalories = "";
+    String mealType = "";
+    Date dateToConsume ;
+
+    meal(String name, String description, int totalCalories, 
+    String mealType, Date dateToConsume){
+        this.name = name;
+        this.description = description;
+        this.totalCalories = totalCalories;
+        this.mealType = mealType;
+        this.dateToConusme = dateToConsume;
+    }
+    public ingredient addingredient(){
+
+    }
+    public void deleteIngredient(){
+
+    }
+}
