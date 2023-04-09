8bc813a9aac0c99bf13134392d807bf522349c00
aidanwilliamson00 <127347971+aidanwilliamson00@users.noreply.github.com>
2023-04-09 18:05:01 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,4 +1,11 @@
 //Account information that is populated by the controller class for app instances
-class Account {
 
+//We need to define how we want to format some of this information to store/retrieve from server.
+public class Account {
+    private int accountID;
+    private String username;
+    private String password;
+    private Workout[] workouts;
+    private MealPlan[] mealplans;
+    private Goal[] goals;
 }
